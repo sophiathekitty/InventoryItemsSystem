db90b38252145439faa2f75a03a52e327486c560
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-05-10 23:44:46 -0600

    commenting

--- Scripts/Crafter.cs
+++ Scripts/Crafter.cs
@@ -18,7 +18,12 @@ public class Crafter : MonoBehaviour {
         // if the can't craft
         if (lockCraft != null && lockCraft == false)
             return;
-		
+
+        // go through all the recipies and work on crafting them
+        foreach (CrafterSlot recipe in recipes)
+        {
+            recipe.Craft(inventory, Time.deltaTime);
+        }
 	}
 
     [System.Serializable]
@@ -28,22 +33,32 @@ public class Crafter : MonoBehaviour {
         public float timeCrafting;
         [System.NonSerialized]
         public bool crafting;
+        // what percent of the craf time does this delta time represent
         public float TimePercent(float deltaTime)
         {
                 return deltaTime / recipe.craft_time;
         }
-
+        // craft a recipe
         public void Craft(Inventory inventory, float deltaTime)
         {
-            if (!crafting)
+            // return if we're not crafting this recipe
+            if (!crafting && !recipe.autoCraft)
                 return;
+            // add delta t
             timeCrafting += deltaTime;
-            int i;
-            for (i = 0; i < recipe.needs.Length; i++)
+            // use up the needs
+            for (int i = 0; i < recipe.needs.Length; i++)
                 inventory.RemoveItems(recipe.needs[i].type, recipe.needs[i].amount * TimePercent(deltaTime));
-            for (i = 0; i < recipe.makes.Length; i++)
+            // add the makes
+            for (int i = 0; i < recipe.makes.Length; i++)
                 inventory.AddItems(recipe.makes[i].type, recipe.makes[i].amount * TimePercent(deltaTime));
-
+            // are we done?
+            if(timeCrafting >= recipe.craft_time)
+            {
+                timeCrafting = 0;
+                if (!recipe.autoCraft)
+                    crafting = false;
+            }
         }
     }
 }
