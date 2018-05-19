df1dc8de3e20f06ce4ead5709f9f116927ba8b21
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-05-19 07:52:45 -0600

    unity meta data is good

--- Scripts/PlayerItemType.cs
+++ Scripts/PlayerItemType.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Items/Player Item Type")]
 public class PlayerItemType : ItemType {
+    public string verb_future;
+    public string verb_present;
+    public string verb_past;
     public GameObject prefab;
     public int width = 1;
     public int height = 1;
